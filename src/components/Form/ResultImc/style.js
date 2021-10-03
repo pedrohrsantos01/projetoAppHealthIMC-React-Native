2352eff846165a8cda9ef983f01a018c84c50d4a
Pedro Santos <pedrohrsantos01@gmail.com>
2021-10-03 14:22:51 -0300

    Estilizando o Resultado do Imc

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    resultImc:{
+        flex: 1,
+        marginTop: 15,
+        paddingTop: 60,
+        borderRadius: 50,
+        alignItems:"center",
+        width:"100%",
+        fontSize: 48,
+        color: '#FF0043',
+        fontWeight: 'bold'
+    },
+    numberImc: {
+        fontSize: 48,
+        color: '#FF0043',
+        fontWeight: 'bold'
+    },
+    information: {
+        fontSize: 18,
+        color: '#FF0043',
+        fontWeight: 'bold'
+    },
+    classImc: {
+        fontSize: 15,
+        color:'#FF00',
+        fontWeight: 'bold'
+    }
+
+})
+
+export default styles
